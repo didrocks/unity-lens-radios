baf398615a0ec944af72eee267a493248965febd
Didier Roche <didrocks@ubuntu.com>
2012-06-13 17:21:55 +0200

    more tests, getting a connection and json parser

--- private_lib/onlineradioinfo.py
+++ private_lib/onlineradioinfo.py
@@ -18,11 +18,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#import json
+import json
 import locale
-#from urllib.parse import urlencode
-#from urllib.request import urlopen, Request
-#from urllib.error import HTTPError, URLError
+import logging
+import urllib
+
+_log = logging.getLogger(__name__)
 
 
 def singleton(cls):
@@ -34,6 +35,14 @@ def singleton(cls):
             instances[cls] = cls(*args, **kwargs)
         return instances[cls]
     return getinstance
+
+
+class ConnectionError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
 
 
 @singleton
@@ -55,9 +64,49 @@ class OnlineRadioInfo(object):
             try:
                 language = locale.setlocale(locale.LC_MESSAGES, '').split('_')[0]
             except locale.Error:
+                _log.debug('Current local {0} is not supported, falling back to en'.format(language))
                 language = 'en'
-        self._language = language
-        self.radio_base_url = self.MAIN_URLS.get(self._language, self.MAIN_URLS['en'])
+        self.radio_base_url = self.MAIN_URLS.get(language, self.MAIN_URLS['en'])
+        _log.debug('Selected radio for {0} language: {1}'.format(language, self.radio_base_url))
 
     def __str__(self):
-        return('{0}, using lang: {1}'.format(repr(self), self._language))
+        return('{0}, using radio: {1}'.format(repr(self), self.radio_base_url))
+
+    def get_category_types(self):
+        '''returns a list of possible values of category_types
+
+        This can be used  by get_categories_by_category_type(category_type)
+        and get_stations_by_category(category_type, category_value)'''
+
+        _log.debug('return category types')
+        return self.VALID_CATEGORY_TYPES
+
+    def _get_json_result_for_parameters(self, path, **parameters):
+        '''Get a json resulting object from the selected radio.
+
+        path represents the url path to get the request
+        parameters are optional parameters given as GET param to the request'''
+
+        url = '{website}/{path}?{parameters}'.format(website=self.radio_base_url, path=path,
+                                                     parameters=urllib.parse.urlencode(parameters))
+        req = urllib.request.Request(url)
+
+        try:
+            _log.debug('Contacting {0}'.format(url))
+            response = urllib.request.urlopen(req)
+            encoding = response.headers.get_content_charset()
+            result = response.readall().decode(encoding)
+        except (urllib.error.HTTPError, urllib.error.URLError) as error:
+            _log.warning('Get a networking error: {0}'.format(error))
+            raise ConnectionError(error)
+
+        try:
+            json_result = json.loads(result)
+            _log.debug('Connection successfully completed done ({0} bytes) and returning: {1}'.format(len(result), json_result))
+        except (ValueError, TypeError) as error:
+            _log.warning("Couldn't convert the result into json: {0}. The Error is: {1}".format(result, error))
+            raise ConnectionError(error)
+
+        return json_result
+
+
--- private_lib/tests/test_onelineradioinfo.py
+++ private_lib/tests/test_onelineradioinfo.py
@@ -17,13 +17,20 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from mock import patch, Mock
 import os
 import unittest
+import urllib
 
-from ..onlineradioinfo import singleton, OnlineRadioInfo
+from ..onlineradioinfo import singleton, OnlineRadioInfo, ConnectionError
 
 
-class OnlineRadioInfoTests(unittest.TestCase):
+class OnlineRadioInfoTestsCommon(unittest.TestCase):
+
+    def setUp(self):
+        # create the singleton. Don't call the super method for children if they need
+        # to create the singleton with other parameters
+        self.radioinfo = OnlineRadioInfo()
 
     def tearDown(self):
         # remove the current singleton
@@ -35,17 +42,23 @@ class OnlineRadioInfoTests(unittest.TestCase):
         except KeyError:
             pass
 
+
+class OnlineRadioInfoMainTests(OnlineRadioInfoTestsCommon):
+
     def test_is_singleton(self):
         '''Test that OnlineRadioInfoTests is a singleton'''
-        radioinfo = OnlineRadioInfo()
-        self.assertEqual(radioinfo, OnlineRadioInfo())
+        self.assertEqual(self.radioinfo, OnlineRadioInfo())
+
+    def test_get_category(self):
+        '''Test that get category is reporting what we need'''
+        self.assertEqual(self.radioinfo.get_category_types(), self.radioinfo.VALID_CATEGORY_TYPES)
 
 
-class OnelineRadioInfoLangTests(OnlineRadioInfoTests):
+class OnlineRadioInfoLangTests(OnlineRadioInfoTestsCommon):
 
     def setUp(self):
         '''Setup a known lang environnement by default'''
-        #super.setUp()
+        # don't call super().setup() here as we want to create our own object with different parameters
         self.system_lang = os.environ['LANG']
         os.environ['LANG'] = "fr_FR.UTF-8"
 
@@ -57,24 +70,59 @@ class OnelineRadioInfoLangTests(OnlineRadioInfoTests):
     def test_auto_detect_lang(self):
         '''Test lang detection and linking to the right radio URL'''
         radioinfo = OnlineRadioInfo()
-        self.assertEqual('fr', radioinfo._language)
         self.assertEqual(radioinfo.MAIN_URLS['fr'], radioinfo.radio_base_url)
 
     def test_manual_lang(self):
         '''Test manual lang set and linking to the right radio URL'''
-        radioinfo = OnlineRadioInfo(language='fr')
-        self.assertEqual('fr', radioinfo._language)
-        self.assertEqual(radioinfo.MAIN_URLS['fr'], radioinfo.radio_base_url)
+        radioinfo = OnlineRadioInfo(language='en')
+        self.assertEqual(radioinfo.MAIN_URLS['en'], radioinfo.radio_base_url)
 
     def test_auto_invalid_lang(self):
         '''Ensure that invalid lang in LANG variable fallback to en radio URL'''
         os.environ['LANG'] = "D"
         radioinfo = OnlineRadioInfo()
-        self.assertEqual('en', radioinfo._language)
         self.assertEqual(radioinfo.MAIN_URLS['en'], radioinfo.radio_base_url)
 
     def test_manual_not_support_lang(self):
         '''Test manual lang provided that isn't supported fallback to en radio URL'''
         radioinfo = OnlineRadioInfo(language='klingon')
-        self.assertEqual('klingon', radioinfo._language)
         self.assertEqual(radioinfo.MAIN_URLS['en'], radioinfo.radio_base_url)
+
+
+class OnlineRadioInfoJsonLineTests(OnlineRadioInfoTestsCommon):
+
+    def _setup_mock_urllib(self, urllibmock):
+        '''Setup the urllib mock object with exceptions and data'''
+        urllibmock.error.HTTPError = urllib.error.HTTPError
+        urllibmock.error.URLError = urllib.error.URLError
+        urllibmock.parse.urlencode = urllib.parse.urlencode
+        urllibmock.request.urlopen().headers.get_content_charset.return_value = "UTF-8"
+        urllibmock.request.urlopen().readall().decode.return_value = '{"foo": [{"bar":"baz"}]}'
+
+    @patch('private_lib.onlineradioinfo.urllib')
+    def test_getting_results(self, urllibmock):
+        '''Test getting regular results back in a json format'''
+        self._setup_mock_urllib(urllibmock)
+        result = self.radioinfo._get_json_result_for_parameters('foo/bar', baz='france', bill='de')
+
+        self.assertEqual(result, {'foo': [{'bar': 'baz'}]})
+        urllibmock.request.Request.assert_called_once_with(self.radioinfo.radio_base_url + "/foo/bar?bill=de&baz=france")
+        self.assertTrue(urllibmock.request.urlopen.called)
+        urllibmock.request.urlopen().headers.get_content_charset.assert_called_once_with()
+        urllibmock.request.urlopen().readall().decode.assert_called_once_with("UTF-8")
+
+    @patch('private_lib.onlineradioinfo.urllib')
+    def test_raising_http_error(self, urllibmock):
+        '''Raising an exception while can't connect to the Internet or getting content'''
+        self._setup_mock_urllib(urllibmock)
+        urllibmock.request.urlopen = Mock(side_effect=urllib.error.HTTPError(Mock(), Mock(), Mock(), Mock(), Mock()))
+
+        urllibmock.request.urlopen = Mock(side_effect=urllib.error.URLError(Mock(), Mock()))
+        self.assertRaises(ConnectionError, self.radioinfo._get_json_result_for_parameters, 'foo/bar', baz='france', bill='de')
+
+    @patch('private_lib.onlineradioinfo.urllib')
+    def test_invalid_json_error(self, urllibmock):
+        '''Raising an exception when receives invalid json content'''
+        self._setup_mock_urllib(urllibmock)
+        urllibmock.request.urlopen().readall().decode.return_value = '{"foo": [{"bar":"baz"}] extraword}'
+        self.assertRaises(ConnectionError, self.radioinfo._get_json_result_for_parameters, 'foo/bar', baz='france', bill='de')
